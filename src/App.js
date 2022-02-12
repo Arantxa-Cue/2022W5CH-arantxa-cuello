d4a3235468928fcffc9d94254a20061d62ec74f5
Arantxa Cuello <arantxa_312@hotmail.com>
2022-02-12 14:26:53 +0100

    api fetched successfully with custom hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./styles.css";
+import useApi from "./hooks/useApi";
 
 function App() {
-  return;
+  const { loading, data } = useApi(`https://series-isdi.herokuapp.com/series`);
+  if (loading) return <h1>Loading</h1>;
+
+  return (
+    <div>
+      <h1>Data fetched successfully</h1>
+      {JSON.stringify(data)}
+    </div>
+  );
 }
 
 export default App;
